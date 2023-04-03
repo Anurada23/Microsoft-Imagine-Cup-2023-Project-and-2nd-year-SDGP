7f147f226856b8897b58a50502aece08c347b35e
Anurada23 <anuradasenaratne23@gmail.com>
2023-04-04 00:07:53 +0530

    test x

--- new frame1/lib/main.dart
+++ new frame1/lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
 		 //home:Scene(),
 		  home: Scaffold(
 		  body: SingleChildScrollView(
-		 	child: Scene1(),//Scene1(),
+		 	child: MyApp5(),//Scene1(),
 
 		 ),
 		  ),
